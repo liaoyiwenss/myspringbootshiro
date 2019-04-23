7f3bb834cce88d65084262df54e54699f5568a03
liaoyiwen <953013493@qq.com>
2019-04-23 16:02:40 +0800

    增加订单，用户地址Service

---
+++ src/main/java/net/wanho/controller/order/OrderController.java
@@ -0,0 +1,17 @@
+package net.wanho.controller.order;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("doorder")
+public class OrderController {
+
+
+
+
+
+
+
+
+}
